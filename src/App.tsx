78777646f99f132461189ed608e33b6242feba60
John Luke <joaolucas.deoliveira56@gmail.com>
2023-09-14 20:35:31 -0300

    feat: create header and textarea's

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
+import { Github } from "lucide-react";
+
 import { Button } from "./components/ui/button";
+import { Textarea } from "./components/ui/textarea";
 
 export function App() {
   return (
-    <>
-      <Button>test</Button>
-    </>
-  )
-}
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl">upload.ia</h1>
 
+        <div className="flex items-center gap-3">
+          <Button variant="outline">
+            <Github className="h-4 w-4 mr-2" />
+            Github
+          </Button>
+        </div>
+      </div>
+
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea
+              className="resize-none p-5 leading-relaxed"
+              placeholder="Inclua o prompt para a IA..."
+            />
+            <Textarea
+              className="resize-none p-5 leading-relaxed"
+              placeholder="Resultado gerado pela IA"
+            />
+          </div>
+
+          <p className="text-small text-muted-foreground">
+            Lembre-se: você pode utilizar a variável{" "}
+            <code className="text-primary">{"{transcription}"}</code> no seu
+            prompt para adicionar o conteúdo da transcrição do video selecionado
+          </p>
+        </div>
+        <aside className="w-80"></aside>
+      </main>
+    </div>
+  );
+}
